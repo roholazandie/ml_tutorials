accd9f3e1954642824f49489b338d62d8cb1f23f
Rohola <hilbert.cantor@gmail.com>
2019-02-26 16:42:47 -0700

    gpu_distribute with only tensorflow

--- tensorflow_tutorials/gpu_distribute.py
+++ tensorflow_tutorials/gpu_distribute.py
@@ -28,7 +28,7 @@ def input_fn(images, labels, epochs, batch_size):
     # Return the dataset. (L)
     return ds
 
-def model_fn():
+def model_fn1():
     inputs = tf.keras.Input(shape=(28, 28, 1))  # Returns a placeholder
     x = tf.keras.layers.Conv2D(filters=32,
                                kernel_size=(3, 3),
@@ -54,6 +54,63 @@ def model_fn():
 
     return model
 
+
+def architecture(inputs, mode, scope='MnistConvNet'):
+    """Return the output operation following the network architecture.
+    Args:
+        inputs (Tensor): Input Tensor
+        mode (ModeKeys): Runtime mode (train, eval, predict)
+        scope (str): Name of the scope of the architecture
+    Returns:
+         Logits output Op for the network.
+    """
+    with tf.variable_scope(scope):
+        inputs = inputs / 255
+        input_layer = tf.reshape(inputs, [-1, 28, 28, 1])
+        conv1 = tf.layers.conv2d(
+            inputs=input_layer,
+            filters=20,
+            kernel_size=[5, 5],
+            padding='valid',
+            activation=tf.nn.relu)
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+        conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=40,
+            kernel_size=[5, 5],
+            padding='valid',
+            activation=tf.nn.relu)
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+        flatten = tf.reshape(pool2, [-1, 4 * 4 * 40])
+        dense1 = tf.layers.dense(inputs=flatten, units=256, activation=tf.nn.relu)
+        dropout = tf.layers.dropout(
+            inputs=dense1, rate=0.5, training=mode==tf.estimator.ModeKeys.TRAIN)
+        dense2 = tf.layers.dense(inputs=dropout, units=10)
+        return dense2
+
+def model_fn(mode, features, labels, params):
+    logits = architecture(features, mode)
+    class_predictions = tf.argmax(logits, axis=-1)
+    predictions = class_predictions
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        loss = tf.losses.sparse_softmax_cross_entropy(
+            labels=tf.cast(labels, tf.int32),
+            logits=logits)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op,
+            predictions=predictions
+        )
+
+
+
 if __name__ == "__main__":
     # the data
     (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
@@ -65,19 +122,9 @@ if __name__ == "__main__":
     test_images = np.asarray(test_images, dtype=np.float32) / 255
     # Convert the test images and add channels
     test_images = test_images.reshape((TEST_SIZE, 28, 28, 1))
-    # How many categories we are predicting from (0-9)
-    LABEL_DIMENSIONS = 10
-    train_labels = tf.keras.utils.to_categorical(train_labels,
-                                                 LABEL_DIMENSIONS)
-    test_labels = tf.keras.utils.to_categorical(test_labels,
-                                                LABEL_DIMENSIONS)
     # Cast the labels to floats, needed later
     train_labels = train_labels.astype(np.float32)
     test_labels = test_labels.astype(np.float32)
-
-
-    # the model
-    model = model_fn()
 
 
     distribution = tf.contrib.distribute.MirroredStrategy()
@@ -86,24 +133,37 @@ if __name__ == "__main__":
 
     time_hist = TimeHistory()
 
-    estimator = tf.keras.estimator.model_to_estimator(model, config=run_config)
+    estimator = tf.estimator.Estimator(model_fn=model_fn,
+                                        config=run_config)
+    # train_spec = tf.estimator.TrainSpec(
+    #     input_fn=lambda: input_fn(train_images,
+    #                                      train_labels,
+    #                                      epochs=1,
+    #                                      batch_size=BATCH_SIZE))
+    #
+    # eval_spec = tf.estimator.EvalSpec(
+    #     input_fn=lambda: input_fn(test_images,
+    #                                      test_labels,
+    #                                      epochs=1,
+    #                                      batch_size=BATCH_SIZE),
+    #     steps=None,
+    #     start_delay_secs=10,  # Start evaluating after 10 sec.
+    #     throttle_secs=30  # Evaluate only every 30 sec
+    # )
+    #
+    # tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec, hooks=[time_hist])
 
-    estimator.train(lambda: input_fn(train_images,
-                                     train_labels,
-                                     epochs=EPOCHS,
-                                     batch_size=BATCH_SIZE),
-                    hooks=[time_hist])
-
-    # classifier = tf.estimator.Estimator(model_fn=,
-    #                                     model_dir=,
-    #                                     config=run_config)
-    #classifier.train(input_fn=input_fn)
+    estimator.train(input_fn=lambda: input_fn(train_images,
+                                         train_labels,
+                                         epochs=1,
+                                         batch_size=BATCH_SIZE), hooks=[time_hist])
 
 
-    estimator.evaluate(lambda: input_fn(test_images,
-                                        test_labels,
-                                        epochs=1,
-                                        batch_size=BATCH_SIZE))
+
+    # estimator.evaluate(lambda: input_fn(test_images,
+    #                                     test_labels,
+    #                                     epochs=1,
+    #                                     batch_size=BATCH_SIZE))
 
     total_time = sum(time_hist.times)
     print("total time with"+ str(NUM_GPUS) + "GPU(s):"+str(total_time)+"seconds")
